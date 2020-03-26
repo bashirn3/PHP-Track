95c426d43922d6f3f27a4c2f8b78a588f7497488
Bashir Nasir Sani <bashnasir@cloud9.ng>
2020-03-26 11:34:42 +0100

    first commit

---
+++ processform.php
@@ -0,0 +1,15 @@
+<?php
+
+$firstName= $_POST['first'];
+$lastName= $_POST['last-name'];
+$email= $_POST['email'];
+$gender=$_POST['gender'];
+$checked=$_POST['terms'];
+$message=$_POST['message'];
+
+$content= " Name: $firstName $lastName \r\n Email: $email \r\n Gender: $gender\r\n Message: $message";
+$file = $firstName .'-'.$lastName .'.txt';
+echo 'Message sent succesfully';
+file_put_contents($file, $content) or die('Cannot wtite to file');
+
+?>
